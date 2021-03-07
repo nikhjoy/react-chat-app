8df6e0503cdc6a157c52849c333edd6f2acbb14d
Nikhil Joy <nikh.liberty77@gmail.com>
2021-03-08 02:11:19 +0400

    app and chat dashroom created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from 'react-chat-engine'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine
+        height="100vh"
+        projectID="35ce4708-11b1-43c3-ab76-85c84043f3ef"
+        userName="nikh"
+        userSecret="nikh123"
+        />
+    )
 }
 
-export default App;
+export default App
